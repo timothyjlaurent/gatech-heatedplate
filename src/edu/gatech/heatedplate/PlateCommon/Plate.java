abde6635881fb910e1c700889e0a95849e2432f5
Tiomothy Laurent <timothyjlaurent@gmail.com>
2014-09-15 08:47:47 -0700

    Made Abstract Plate class and IterationMessage.

--- src/edu/gatech/heatedplate/PlateCommon/Plate.java
+++ src/edu/gatech/heatedplate/PlateCommon/Plate.java
@@ -2,5 +2,13 @@ package edu.gatech.heatedplate.PlateCommon;
 
 public abstract class Plate {
 	
+	public abstract IterationMessage InitializePlate(int height, 
+			int width, 
+			double top, 
+			double right, 
+			double bottom, 
+			double left);
+	
+	public abstract IterationMessage Iterate();
 	
 }
